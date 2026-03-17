579f98a9a4c7123a39b2aa35734fc635f4130062
varunpat789 <varunpat789@gmail.com>
2026-03-16 22:49:27 -0400

    fix: servo control mux

--- asne_pkg/asne_pkg/servo_control_node.py
+++ asne_pkg/asne_pkg/servo_control_node.py
@@ -4,6 +4,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
 from math import pi
+from asne_interfaces.msg import State
 
 
 SERVO_RANGE = 120 * (pi / 180)  # radians
@@ -15,14 +16,51 @@ class ServoControlNode(Node):
     def __init__(self):
         super().__init__("servo_control_node")
 
-        self.heading_sub = self.create_subscription(Float64, "asne/goal_heading", 10, self.heading_callback)
-        self.servo_pub = self.create_publisher(Float64, "asne/servo_angle", 10)
+        self.manual_heading_sub = self.create_subscription(Float64, "/asne/heading/manual", 10, self.heading_manual_callback)
+        self.auto_heading_sub = self.create_subscription(Float64, "/asne/heading/autonomous", 10, self.heading_auto_callback)
+        self.state_sub = self.create_subscription(State, "/asne/state", 10, self.state_callback)
 
-    def heading_callback(self, msg: Float64):  # rads
+        self.servo_pub = self.create_publisher(Float64, "/asne/servo_angle", 10)
+
+        self.desired_manual_heading: float = 0.0
+        self.desired_autonomous_heading: float = 0.0
+
+        self.timer = self.create_timer(0.01, self.timer_callback)
+
+        self.state: State | None = None
+
+    def heading_manual_callback(self, msg: Float64):  # rads
         servo_angle = msg.data  # TODO: convert desired heading to servo angle
         servo_angle = max(min(SERVO_RANGE, servo_angle), -SERVO_RANGE)
 
+        self.desired_manual_heading = servo_angle
+
+    def heading_auto_callback(self, msg: Float64):  # rads
+        servo_angle = msg.data  # TODO: convert desired heading to servo angle
+        servo_angle = max(min(SERVO_RANGE, servo_angle), -SERVO_RANGE)
+
+        self.desired_autonomous_heading = servo_angle
+        
+    def state_callback(self, msg: State):
+        self.state = msg.state
+
+    def timer_callback(self):
         servo_msg: Float64 = Float64()
+
+        match self.state:
+            case State.MANUAL:
+                servo_angle = self.desired_manual_heading
+            case State.AUTONOMOUS:
+                servo_angle = self.desired_autonomous_heading
+            case State.STATIONARY:
+                servo_angle = 0.0
+            case State.RC_ESTOP:
+                servo_angle = 0.0
+            case State.GPS_ESTOP:
+                servo_angle = 0.0
+            case _:
+                servo_angle = 0.0
+
         servo_msg.data = servo_angle  # rads
         self.servo_pub.publish()
 
