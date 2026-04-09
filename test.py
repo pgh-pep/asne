b27e722cdf475d63df855645fa2b53a45de4496b
varunpat789 <varunpat789@gmail.com>
2026-04-08 21:16:09 -0400

    fv

---
+++ test.py
@@ -0,0 +1,20 @@
+import serial
+import time
+
+ser = serial.Serial("/dev/ttyUSB3", 115200, timeout=1)
+
+while True:
+    time.sleep(2)
+
+    ser.write(b"SA:135\n")
+
+    time.sleep(2)
+
+    ser.write(b"SA:0\n")
+
+    time.sleep(2)
+
+    ser.write(b"SA:270\n")
+
+while ser.in_waiting:
+    print(ser.readline().decode())
